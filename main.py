b3504cac409a723373d4b587ebc7301593702ba5
Manuel Morales <p513manu@gmail.com>
2021-10-28 07:51:01 -0500

    feat: return base64 text file for pdf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, send_file
 from flask_cors import CORS
-from pdf import merge_b64_pdf_files
+from pdf import merge_b64_pdf_files, pdf_to_b64
 
 app = Flask(__name__)
 CORS(app)
@@ -14,5 +14,10 @@ def merge():
     files = data.get('files')
     merged_file = merge_b64_pdf_files(files)
     merged_file.seek(0)
+    mime ='application/pdf' 
 
-    return send_file(merged_file, as_attachment=True, attachment_filename="merged.pdf", mimetype='application/pdf')
+    if data.get('base64') is not None:
+        merged_file = pdf_to_b64(merged_file.read())
+        mime = 'application/text'
+
+    return send_file(merged_file, as_attachment=True, attachment_filename="merged.pdf", mimetype=mime)
--- pdf.py
+++ pdf.py
@@ -1,5 +1,5 @@
 from PyPDF2 import PdfFileMerger, PdfFileReader
-from io import BytesIO
+from io import BytesIO, StringIO
 import base64
 
 def merge_b64_pdf_files(file_strings):
@@ -14,4 +14,13 @@ def merge_b64_pdf_files(file_strings):
     merged_object.write(output)
 
     return output
+
+def pdf_to_b64(bstring):
+    bio  = BytesIO()
+    b64string = base64.b64encode(bstring)
+
+    bio.write(b64string)
+
+    return bio
+
 
